e783420e315f73736b96fa3f22780471d517a08b
Peter McPhail <mcphailpeter@gmail.com>
2022-05-29 17:52:49 -0500

    Starting code for map info popup

--- js/main.js
+++ js/main.js
@@ -83,14 +83,90 @@
 
 })(jQuery);
 
-//Google maps functions
+var locations = [
+    {
+        "title": 'Petes Garden',
+        "lat": '44.0497020484571',
+        "lng": ' -91.6754105152892',
+        "img-path":"",
+        "uses-pesticides":false,
+        "pick-your-own":true,
+        "is-limit":true,
+        "description": 'Various fruits ready to be picked.'
+    },
+    {
+        "title": 'Bills Curb Garden',
+        "lat": '44.05096281797642',
+        "lng": '-91.67619372031038',
+        "img-path":"",
+        "uses-pesticides":false,
+        "pick-your-own":true,
+        "is-limit":true,
+        "description": 'Offering mainly lettuce and turnips.'
+    },
+    {
+        "title": 'Laylas Backyard',
+        "lat": '44.05124327433896',
+        "lng": '-91.67982287097426',
+        "img-path":"",
+        "uses-pesticides":false,
+        "pick-your-own":true,
+        "is-limit":true,
+        "description": 'Lots of corn ready to harvest.  Also offering tomatoes and carrots.'
+    },
+    {
+        "title": 'Larrys Front Garden',
+        "lat": '44.04721529953577',
+        "lng": '-91.67684025459225',
+        "img-path":"",
+        "uses-pesticides":false,
+        "pick-your-own":true,
+        "is-limit":true,
+        "description": 'I usually have plenty of potatoes included sweet potatoes.'
+    },
+    {
+        "title": 'Sanjays Veggies',
+        "lat": '44.04721529953577',
+        "lng": '-91.64334841278864',
+        "img-path":"",
+        "uses-pesticides":false,
+        "pick-your-own":true,
+        "is-limit":true,
+        "description": 'Come get it! Bock choy, radishes, napa cabbage and sweet peas'
+    },
+    {
+        "title": 'Janes Fruits and More',
+        "lat": '44.04855323590796',
+        "lng": '-91.63001783336748',
+        "img-path":"",
+        "uses-pesticides":false,
+        "pick-your-own":true,
+        "is-limit":true,
+        "description": 'I carry peaches, strawberries, plums and apricots'
+    }
+    ];
 
+var contentString = '<div class="card">' +
+'<img class="card-img-top" src="https://images.unsplash.com/photo-1594498653385-d5172c532c00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGdhcmRlbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="Card image">' +
+'<div class="card-body">'+
+  '<h4 class="card-title">Card title</h4>'+
+  '<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card content.</p>'+
+  '<div>'+
+  '<i class="fa-solid fa-xmark"></i> Pesticides'+
+  '<i class="fa-solid fa-xmark"></i> Pick your own'+
+  '<i class="fa-solid fa-check"></i> Limit per person'+
+  '</div>'+
+'<button type="button" class="btn btn-outline-secondary btn-block">Get Directions</button>'+
+'</div>'+
+'</div>'
+
+//Google maps functions
 function initMap() {
     // locate users coordinates to find on map
     if ("geolocation" in navigator) {
         console.log("geolocation exists for this browser")
         navigator.geolocation.getCurrentPosition(function (p) {
-            locateUser(p.coords.latitude, p.coords.longitude);
+            addMarkers(p.coords.latitude, p.coords.longitude);
         }, function (e) {
             ipLookup();
         });
@@ -100,54 +176,33 @@ function initMap() {
 
 };
 
-function locateUser(latitude, longitude, additional) {
-    console.log(latitude);
-    console.log(longitude);
-    var position = latitude + "," + longitude;
-    var g_map = new google.maps.Map(document.getElementById("map"), {
-        center: new google.maps.LatLng(latitude, longitude),
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
+function addMarkers(lat, lng) {
+    var mapOptions = {
+        center: new google.maps.LatLng(lat,lng),
         zoom: 15,
-    });
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var infoWindow = new google.maps.InfoWindow();
+    var latlngbounds = new google.maps.LatLngBounds();
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-    addMarkers(g_map);
-}
-
-
-function ipLookup() {
-    $.get('https://api.userinfo.io/userinfos', function (r) {
-        showUserDetails(r.position.latitude, r.position.longitude, r);
-    });
-}
-
-function addMarkers(g_map) {
-    console.log("adding markers");
-    var locations = [
-        ['Loc A', 44.0497020484571, -91.6754105152892, 1],
-        ['Loc B', 44.05096281797642, -91.67619372031038, 2],
-        ['Loc C', 44.05124327433896, -91.67982287097426, 3],
-        ['Loc E', 44.05198352666992, -91.67684025459225, 4],
-        ['Loc F', 44.04721529953577, -91.64334841278864, 5],
-        ['Loc G', 44.04855323590796, -91.63001783336748, 6]
-    ];
-
-    var marker, i;
-
-    for (i = 0; i < locations.length; i++) {
-        marker = new google.maps.Marker({
-            position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-            map: g_map,
-            icon: 'img/gro-marker.svg',
-            animation: google.maps.Animation.DROP
-
+    for (var i = 0; i < locations.length; i++) {
+        var data = locations[i]
+        var myLatlng = new google.maps.LatLng(data.lat, data.lng);
+        var marker = new google.maps.Marker({
+            position: myLatlng,
+            map: map,
+            title: data.title
         });
-
-        google.maps.event.addListener(marker, 'click', (function(marker, i){
-            return function() {
-                infowindow.setContent(locations[i][0]);
-                infowindow.open(map, marker);
-            }
-        })(marker, i));
+        (function (marker, data) {
+            google.maps.event.addListener(marker, "mouseover", function (e) {
+                infoWindow.setContent(contentString);
+                infoWindow.open(map, marker);
+            });
+        })(marker, data);
+        latlngbounds.extend(marker.position);
     }
-    
+    var bounds = new google.maps.LatLngBounds();
+    // map.setCenter(latlngbounds.getCenter());
+    map.fitBounds(latlngbounds);
 }
